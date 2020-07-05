d121e156c8c8dc89994ed35cdf4f7d14fa986aba
Pavel <sur-pavel@mail.ru>
2020-07-05 21:16:46 +0300

    delete file property

--- src/main/java/ru/surpavel/bugtrackingsystem/entity/Project.java
+++ src/main/java/ru/surpavel/bugtrackingsystem/entity/Project.java
@@ -16,10 +16,6 @@ public class Project {
     @Column(unique = true)
     private String title;
 
-    @Size(max = 100)
-    @Column(unique = true)
-    private String file;
-
     public Long getId() {
         return id;
     }
@@ -36,19 +32,10 @@ public class Project {
         this.title = title;
     }
 
-    public String getFile() {
-        return file;
-    }
-
-    public void setFile(String file) {
-        this.file = file;
-    }
-
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((file == null) ? 0 : file.hashCode());
         result = prime * result + ((id == null) ? 0 : id.hashCode());
         result = prime * result + ((title == null) ? 0 : title.hashCode());
         return result;
@@ -63,11 +50,6 @@ public class Project {
         if (getClass() != obj.getClass())
             return false;
         Project other = (Project) obj;
-        if (file == null) {
-            if (other.file != null)
-                return false;
-        } else if (!file.equals(other.file))
-            return false;
         if (id == null) {
             if (other.id != null)
                 return false;
