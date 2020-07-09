bfc85f0ea6d5b8760d68abbb9d50b3d96ab6befa
Pavel <sur-pavel@mail.ru>
2020-07-09 10:32:03 +0300

    improve tests

--- src/test/java/ru/surpavel/bugtrackingsystem/ProjectRestControllerIntegrationTest.java
+++ src/test/java/ru/surpavel/bugtrackingsystem/ProjectRestControllerIntegrationTest.java
@@ -16,7 +16,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -77,11 +76,11 @@ public class ProjectRestControllerIntegrationTest {
     }
     @Test
     public void createProject() throws Exception {
-        
+        String title = "third";
         ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        String json = ow.writeValueAsString(new Project("second"));
+        String json = ow.writeValueAsString(new Project(title));
         mockMvc.perform(post("/projects").content(json)
                 .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk()).andExpect(MockMvcResultMatchers.jsonPath("second").exists());
+                .andExpect(status().isOk()).andExpect(MockMvcResultMatchers.jsonPath(title).exists());
     }
 }
